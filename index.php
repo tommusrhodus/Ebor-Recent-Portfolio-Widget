87814ee5f662544768dd3c2bed2b760d555daddd
Tom Rhodes <hello@madeinebor.com>
2013-08-20 10:10:11 +0100

    Removed Updater

--- index.php
+++ index.php
@@ -9,38 +9,6 @@ Author: TommusRhodus
 Author URI: http://www.madeinebor.com
 */	
 
-
-/*-----------------------------------------------------------------------------------*/
-/*	PLUGIN UPDATER
-/*-----------------------------------------------------------------------------------*/
-
-add_action( 'init', 'ebor_recent_portfolio_update' );
-function ebor_recent_portfolio_update() {
-
-	include_once 'updater.php';
-
-	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
-
-		$config = array(
-			'slug' => plugin_basename( __FILE__ ),
-			'proper_folder_name' => 'Ebor-Popular-Posts',
-			'api_url' => 'https://api.github.com/repos/tommusrhodus/Ebor-Recent-Portfolio-Widget',
-			'raw_url' => 'https://raw.github.com/tommusrhodus/Ebor-Recent-Portfolio-Widget/master',
-			'github_url' => 'https://github.com/tommusrhodus/Ebor-Recent-Portfolio-Widget',
-			'zip_url' => 'https://github.com/tommusrhodus/Ebor-Recent-Portfolio-Widget/archive/master.zip',
-			'sslverify' => true,
-			'requires' => '3.6',
-			'tested' => '3.6',
-			'readme' => 'README.md',
-			'access_token' => '',
-		);
-
-		new WP_GitHub_Updater( $config );
-
-	}
-
-}
-
 /*-----------------------------------------------------------------------------------*/
 /*	ENQUEUE STYLING
 /*-----------------------------------------------------------------------------------*/
